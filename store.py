98cae1678f388147ba5224e39f75c686bc8f860b
Erica-99 <turbocake17@gmail.com>
2025-04-04 19:47:15 +1100

    Created initial framework

---
+++ store.py
@@ -0,0 +1,30 @@
+""" STORE """
+""" This script should:
+    - Handle storage, stowing, and retrieval of any data in long-term storage.
+    - Tag hazard frames with GPS and time then store them.
+    - Storage of any other permanent files which the codebase needs access to (config, maybe frontend files, etc.)"""
+
+
+def tag_and_store():
+    return NotImplementedError
+
+def get_gps():
+    return NotImplementedError
+
+def store_image():
+    return NotImplementedError
+
+def get_image():
+    return NotImplementedError
+
+def get_model():
+    return NotImplementedError
+
+def save_model():
+    return NotImplementedError
+
+def get_file():
+    return NotImplementedError
+
+def save_file():
+    return NotImplementedError
