967570df840385ba831e42ec4aaf02a7303b32e3
Erica-99 <turbocake17@gmail.com>
2025-04-10 21:45:35 +1000

    Implemented GPS tagging

--- camera_feed.py
+++ camera_feed.py
@@ -2,7 +2,8 @@
 """ This script should:
     - Handle receiving the camera feed.
     - Bundle camera feed into frames to find key ones.
-    - Selecting key frames to send further down the pipeline."""
+    - Selecting key frames to send further down the pipeline.
+    - Allow the camera to give GPS data upon request. """
 
 
 class SingleFrame():
@@ -17,3 +18,9 @@ def get_key_frame():
 
 def pipe_frame_to_processing():
     return NotImplementedError
+
+def get_camera_gps():
+    # Implement proper method once camera is connected.
+    # For now, return coordinates from somewhere in Melbourne.
+    location = (-37.814167, 144.963056)
+    return location
--- train_test.py
+++ train_test.py
@@ -2,14 +2,21 @@
 """ This script should:
     - Be used for training and finetuning models during development.
     - Be used for evaluating AI models alone.
-    - Be used testing new techniques in regards to the AI models."""
+    - Be used testing new techniques in regards to the AI models.
+    - Be used for testing various things that need testing."""
 
+import store
+from PIL import Image
 
 # ===================== SETUP =====================
 
+def test_gps_tag():
+    test_image = Image.open("data/test.jpg")
 
+    saved = store.tag_and_store(test_image)
 
-
+    check_image = Image.open(saved)
+    print(check_image.getexif())
 
 # ===================== TRAIN =====================
 
@@ -20,5 +27,5 @@
 # ===================== EVALUATION =====================
 
 
-
+#test_gps_tag()
 
